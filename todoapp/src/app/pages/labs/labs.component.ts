67f28bd1d6b3f07c3d4e166abcf7d313dc2a0f66
Yorch <jorge.bethancourt04@hotmail.com>
2025-01-08 16:24:52 -0500

    Se añadieron ajustes y controles

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-  welcome = 'Bienvenido a mi primera aplicaicón con Angular'
-  tasks = [
+  welcome = 'Bienvenido a mi primera aplicación con Angular'
+  tasks = signal([
     'Instalar el Angular CLI',
     'Crear proyecto',
-    'Crear componentes'
-  ]
+    'Crear componentes',
+    'Crear servicio'
+  ]);
+  name = signal('Jorge');
+  age = 23;
+  disabled = true
+  img = 'https://w3schools.com/howto/img_avatar.png'
 
+  person = {
+    name: 'Jorge',
+    age: '23',
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+
+  clickHandler(){
+    alert('Hola')
+  }
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
